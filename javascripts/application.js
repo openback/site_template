15ecf2aa1010e91a9f63b291f2c3136bf372f6fb
Timothy Caraballo <openback@openeee.(none)>
2008-12-09 18:44:53 -0500

    First commit

---
+++ javascripts/application.js
@@ -0,0 +1,45 @@
+// From: http://cass-hacks.com/articles/code/js_url_encode_decode/
+// Explanation: http://cass-hacks.com/articles/discussion/js_url_encode_decode/
+function URLEncode(clearString) {
+  var output = '';
+  var x = 0;
+  clearString = clearString.toString();
+  var regex = /(^[a-zA-Z0-9_.]*)/;
+  while (x < clearString.length) {
+    var match = regex.exec(clearString.substr(x));
+    if (match != null && match.length > 1 && match[1] != '') {
+    	output += match[1];
+      x += match[1].length;
+    } else {
+      if (clearString[x] == ' ')
+        output += '+';
+      else {
+        var charCode = clearString.charCodeAt(x);
+        var hexVal = charCode.toString(16);
+        output += '%' + ( hexVal.length < 2 ? '0' : '' ) + hexVal.toUpperCase();
+      }
+      x++;
+    }
+  }
+  return output;
+}
+
+function URLDecode(encodedString) {
+  var output = encodedString;
+  var binVal, thisString;
+  var myregexp = /(%[^%]{2})/;
+  while ((match = myregexp.exec(output)) != null
+             && match.length > 1
+             && match[1] != '') {
+    binVal = parseInt(match[1].substr(1),16);
+    thisString = String.fromCharCode(binVal);
+    output = output.replace(match[1], thisString);
+  }
+  return output;
+}
+
+/******************************************************************************/
+Event.observe(window, 'load', function() {
+
+});
+
